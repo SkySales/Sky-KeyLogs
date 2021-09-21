ca745e641f6496c49c8db8dc3105fd8b7bf2d963
$ky <86482404+SkySales@users.noreply.github.com>
2021-09-21 09:33:18 +0800

    Add files via upload

---
+++ sky_keylogs/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+Description 'SIMPLE LOGS FOR BLACKLISTED KEYS'
+Author 'SKY X LOWKEY > SKY SALES'
+
+version '1.1.0'
+
+client_script {
+
+    'logs/cl_keylogs.lua',
+
+}
+server_script {
+
+    'logs/sv_keylogs.lua',
+
+}
